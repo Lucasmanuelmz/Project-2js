c4e3dc2cc7543bab981bb33e5deeb300b0741bf9
Lucasmanuelmz <155545753+Lucasmanuelmz@users.noreply.github.com>
2024-01-19 09:38:57 +0200

    Adicionei comentários no arquivo

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
+//Nesta função defino as opções e a escolha aleatória do computador as opções
 function getComputerChoice(playerSelection) {
-    let options = ['Rock', 'Paper', 'Scissors'];
-    let computerSelection = options[Math.floor(Math.random() * 3)] 
+    let options = ['Rock', 'Paper', 'Scissors'];//Opções
+    let computerSelection = options[Math.floor(Math.random() * 3)] //Escolha aleatória do computador
     console.log( `Você escolheu: ${playerSelection} Computador escolheu: 
-    ${computerSelection} ${playRound(playerSelection, computerSelection)}`);
+    ${computerSelection} ${playRound(playerSelection, computerSelection)}`); // imprimo o resultado no console
     
 }
-
+//Defino a lógica e as regras do jogo
 function playRound(playerSelection, computerSelection) {
     if(playerSelection === computerSelection) {
         return "Empate!"
     } else if (playerSelection === "Rock" && computerSelection === "Scissors" || 
             playerSelection === "Scissors" && computerSelection === "Paper" || 
             playerSelection === "Paper" && computerSelection === "Rock") {
-                return `Você ganhou!`;
+                return "Você ganhou!";
             } else {
-               return 'Você perdeu!';
+               return "Você perdeu!";
             } 
   }
 
-const playerSelection = "Rock";
+const playerSelection = "scissors"; //Seleção da opção do jogador
 const computerSelection = getComputerChoice(playerSelection);
-playRound(playerSelection, computerSelection);
+playRound(playerSelection, computerSelection);//invoquei a função playRound aqui
